9048319d3e09b58cb25cbf609dc3c3f9ff7eb1d1
Marciofit88 <marcioaquinofit@gmail.com>
2020-02-19 15:48:37 -0300

    adicionei arquivo via upload

---
+++ Hello Github/src/br/com/joseassis/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.joseassis.git;
+
+/**
+ * @author MARCIO
+ * Exemplo de integração do Eclipse com o Github
+ *
+ */
+public class HelloGit {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
